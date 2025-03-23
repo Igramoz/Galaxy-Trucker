2a955701c5f460cf5573f5f8f0dfbb9946ca9580
Marco Magri <81182912+Igramoz@users.noreply.github.com>
2025-03-23 21:41:10 +0100

    Update coordinate e util.Util.SIZE
    -messo il controllo in coordinate x e y che devono essere compresi tra [10 e 0]

--- src/model/Coordinate.java
+++ src/model/Coordinate.java
@@ -1,13 +1,26 @@
 package model;
 
+//import della costante size
+
+import util.Util;
+
 public class Coordinate {
     private final int x;
     private final int y;
 
     public Coordinate(int x, int y) {
+    	
+    	//controllo se le coordinate sono comprese tra 0 e 10 (compresi)
+    	
+    	if((x < 0 || x > Util.SIZE) || (y<0 | y>Util.SIZE)) {
+    		
+    		throw new IllegalArgumentException("Le coordinate devono essere comprese tra [0 e 10]");
+    	}
+    	
         this.x = x;
         this.y = y;
     }
+    
 
     public int getX() {
         return x;
