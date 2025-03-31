3117d23d7150a0b0bf2caa3085c5c0d9ae3cba37
8TB-Available <mikimegov@gmail.com>
2025-03-31 19:57:48 +0200

    Update TipoPedina.java

--- src/model/equipaggio/TipoPedina.java
+++ src/model/equipaggio/TipoPedina.java
@@ -1,72 +1,32 @@
 package model.equipaggio;
 
-import componenti.TipoComponente;
-import java.util.Objects;
-
 public class TipoPedina {
 
     public enum Colore {
-        BIANCO(255, 255, 255),  // Astronauta
-        VIOLA(128, 0, 128),     // Alieno viola
-        MARRONE(139, 69, 19);   // Alieno marrone
-
-        private final int r, g, b;
-
-        Colore(int r, int g, int b) {
-            this.r = r;
-            this.g = g;
-            this.b = b;
-        }
-
-        public int getR() {
-            return r;
-        }
-
-        public int getG() {
-            return g;
-        }
-
-        public int getB() {
-            return b;
-        }
-
-        public String getRGBString() {
-            return "(" + r + ", " + g + ", " + b + ")";
-        }
+        VIOLA,  // Alieno viola
+        MARRONE // Alieno marrone
     }
 
-    private final Colore colore;
-    private final int maxQuantita;
+    public static final TipoPedina ALIENO_VIOLA = new TipoPedina(Colore.VIOLA);
+    public static final TipoPedina ALIENO_MARRONE = new TipoPedina(Colore.MARRONE);
+
+    public final Colore colore; 
 
     public TipoPedina(Colore colore) {
-        this.colore = Objects.requireNonNull(colore, "Il colore non può essere nullo");
-
-        switch (colore) {
-            case BIANCO -> this.maxQuantita = TipoComponente.CABINA_EQUIPAGGIO.getMaxIstanze();
-            case VIOLA -> this.maxQuantita = TipoComponente.SOVRASTRUTTURA_ALIENA_VIOLA.getMaxIstanze();
-            case MARRONE -> this.maxQuantita = TipoComponente.SOVRASTRUTTURA_ALIENA_MARRONE.getMaxIstanze();
-            default -> throw new IllegalArgumentException("Colore non valido");
-        }
+        this.colore = colore;
     }
 
     public Colore getColore() {
         return colore;
     }
 
-    public int getMaxQuantita() {
-        return maxQuantita;
-    }
-
-    public String getRGBString() {
-        return colore.getRGBString();
-    }
-
     @Override
-    public String toString() {
-        return "TipoPedina{" +
-                "colore=" + colore +
-                ", maxQuantita=" + maxQuantita +
-                ", RGB=" + getRGBString() +
-                '}';
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        TipoPedina other = (TipoPedina) obj;
+        return colore == other.colore;
     }
 }
+
+
