987410fb00813aa865d9583a0acea8a0fe2ebd34
Marco Magri <81182912+Igramoz@users.noreply.github.com>
2025-04-02 17:28:31 +0200

    Update CapinaPartenza
    -Sistemata CabinaPartenza extends CabinaDiEquipaggio

--- src/componenti/CabinaPartenza.java
+++ src/componenti/CabinaPartenza.java
@@ -1,66 +1,49 @@
 package componenti;
 
 import java.util.Map;
+import java.util.List;
+import model.equipaggio.TipoPedina;
 
 import model.enums.*;
 
-public class CabinaPartenza extends Componente{ // TODO: exteds CabinaDiEquipaggio
+public class CabinaPartenza extends CabinaDiEquipaggio{
 	
 	private static int istanze = 0;
-	private int equipaggio; // non serve, già il padre Cabina di equipaggio ha tutte le funzionalità che servono a questa classe
 	// TODO: dovremo capire come assegnare il colore a questa cabina
 	
 	
 	public CabinaPartenza(Map<Direzione, TipoTubo> tubiIniziali) {
-		super(TipoComponente.CABINA_PARTENZA, tubiIniziali);
-		if(istanze >= TipoComponente.CABINA_PARTENZA.getMaxIstanze() || checkTubi(tubiIniziali)) {
+		super(tubiIniziali);
+		if (istanze >= TipoComponente.CABINA_PARTENZA.getMaxIstanze() || checkTubi(tubiIniziali)) {
 			throw new IllegalStateException("Limite massimo di istanze raggiunto per CabinaPartenza");
 		}
 		
-		this.equipaggio = 2;
 		incrementaIstanze();
 	}
 	
 	// TODO: questa funzione non va bene, devi sfruttare la funzione del padre che si occupa di asegnare i membri dell'equipaggio
 	// ma prima controlla che i membri dell'equipaggio siano tutti ASTRONAUTI
-	public CabinaPartenza(Map<Direzione, TipoTubo> tubiIniziali, int equipaggio) {
-		super(TipoComponente.CABINA_PARTENZA, tubiIniziali);
+	public CabinaPartenza(Map<Direzione, TipoTubo> tubiIniziali, List<TipoPedina> equipaggioIniziale) {
+		super(tubiIniziali, equipaggioIniziale);
 		if(istanze >= TipoComponente.CABINA_PARTENZA.getMaxIstanze() || checkTubi(tubiIniziali)) {
 			throw new IllegalStateException("Limite massimo di istanze raggiunto per CabinaPartenza");
 		}
-		
-		if(equipaggio > 2 || equipaggio < 0) {
-			throw new IllegalArgumentException("L'equipaggio deve essere compreso tra 1 e 2");
+		for (TipoPedina pedina : equipaggioIniziale) {
+			if(pedina.getColore() != TipoPedina.Colore.ASTRONAUTA) {
+				throw new IllegalStateException("Nella cabina di partenza possono esserci solo astronauti");
+			}
 		}
 		incrementaIstanze();
 	}
+
+	
 	
 	public CabinaPartenza(CabinaPartenza cabinaPartenza) { // costruttore di copia
-		super(cabinaPartenza);
+		this(cabinaPartenza.tubi, cabinaPartenza.getEquipaggio());
 		// TODO: prima di decrementare le istanze, controlla che venga chiamata una funzione che le aggiorna.
 	}
 	
-	// TODO: non serve sapere quanti sono i membri dell'equipaggio, serve sapere quali sono
-	public int getEquipaggio() {
-		return this.equipaggio;
-	}
-	
-	// TODO: il padre ha una fuznione che se ne occupa, questa non serve
-	public boolean decrementaEquipaggio() {
-		if(this.equipaggio > 0) {
-			this.equipaggio--;
-			return true;
-		} return false;
-	}
-	
-	// TODO: il padre ha una fuznione che se ne occupa, questa non serve
-	public boolean incrementaEquipaggio() {
-		if(this.equipaggio < 2) {
-			this.equipaggio++;
-			return true;
-		} return false;
-	}
-		
+
 	@Override
     public CabinaPartenza clone() {    	
     	return new CabinaPartenza(this); 
@@ -79,18 +62,18 @@ public class CabinaPartenza extends Componente{ // TODO: exteds CabinaDiEquipagg
 	}
     
     @Override
-	protected  void incrementaIstanze() {
+	public void incrementaIstanze() {
 		istanze++;
 	}
 
     @Override
-	protected  void decrementaIstanze() {
+	public  void decrementaIstanze() {
 		istanze--;
 	}
     
     
     // TODO: la classe componente si occupa di controllare i tubi, cancella
-    //controllo se tutti i tubi sono di tipo universale
+    
     private boolean checkTubi(Map<Direzione, TipoTubo> tubiIniziali) {
     	
     	for(Direzione direzione : Direzione.values()) {
