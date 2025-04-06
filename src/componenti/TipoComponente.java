75a1bcb2e283fec0dba2cfc9552082e2434a3b7b
Matteo Baccanelli <m.baccanelli1@studenti.unibg.it>
2025-04-06 18:15:36 +0200

    Introdotta la gestione dei colori dei componenti

--- src/componenti/TipoComponente.java
+++ src/componenti/TipoComponente.java
@@ -1,35 +1,46 @@
 package componenti;
 
 public enum TipoComponente {
-	CABINA_EQUIPAGGIO("CE", 17),
-	CABINA_PARTENZA("CP", 4),
-	CANNONE_SINGOLO("CS", 25),
-	CANNONE_DOPPIO("CD", 11),
-	MOTORE_SINGOLO("MS", 21),
-	MOTORE_DOPPIO("MD", 9),
-	SCUDO("S", 8),
-	STIVA("M", 25),
-	STIVA_SPECIALE("MS", 9),
-	VANO_BATTERIA("B", 17),
-	SOVRASTRUTTURA_ALIENA_VIOLA("SAV", 6),
-	SOVRASTRUTTURA_ALIENA_MARRONE("SAM", 6),
-	TUBO("T", 8);
-	// La M di stiva significa Magazzino
+	CABINA_EQUIPAGGIO("CE", 17, "\u001B[38;5;15m"), // Bianco
+	CABINA_PARTENZA("CP", 4, "\u001B[38;5;226m"), // Giallo
+	CANNONE_SINGOLO("CS", 25, "\u001B[38;5;183m"), // Viola
+	CANNONE_DOPPIO("CD", 11, "\u001B[38;5;183m"),
+	MOTORE_SINGOLO("MS", 21, "\u001B[38;5;180m"), // Marrone
+	MOTORE_DOPPIO("MD", 9, "\u001B[38;5;180m"),
+	SCUDO("SC", 8, "\u001B[38;5;34m"), // Verde
+	STIVA("S", 25, "\u001B[38;5;51m"), // Azzurro
+	STIVA_SPECIALE("SS", 9, "\u001B[38;5;203m"), // Rosso
+	VANO_BATTERIA("B", 17, "\u001B[38;5;120m"), // Verde
+	SOVRASTRUTTURA_ALIENA_VIOLA("SAV", 6, "\u001B[38;5;129m"), // Viola scuro
+	SOVRASTRUTTURA_ALIENA_MARRONE("SAM", 6, "\u001B[38;5;137m"), // Marrone scuro
+	TUBO("T", 8, "\u001B[37m"); // Grigio
+
+	private static final String RESET = "\u001B[0m"; // reimposta il colore default di stampa
 
 	private final String sigla;
 	private final int maxIstanze;
+	private final String colore; // è un codice ANSI
 
-	private TipoComponente(String s, int maxIstanze) {
+	private TipoComponente(String s, int maxIstanze, String colore) {
 		sigla = s;
 		this.maxIstanze = maxIstanze;
+		this.colore = colore;
 	}
 
 	@Override
 	public String toString() {
-		return sigla;
+		return colore + sigla + RESET;
 	}
 
 	public int getMaxIstanze() {
 		return maxIstanze;
+	}
+
+	public String getColore() {
+		return colore;
+	}
+
+	public String getSigla() {
+		return sigla;
 	}
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,5 @@
 package main;
 
-
 import servizi.ServizioAssemblaggio;
 import componenti.*;
 import grafica.ConvertitoreGrafica;
@@ -9,25 +8,22 @@ import grafica.GestoreGrafica;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Ciao Mondo!!");
-		
+		for (TipoComponente c : TipoComponente.values()) { // Test funzionamento colori
+			System.out.println(c + " → max: " + c.getMaxIstanze());
+		}
 		ConvertitoreGrafica grafica = new ConvertitoreGrafica();
 		ServizioAssemblaggio servizio = new ServizioAssemblaggio();
 		GestoreGrafica gestoreGrafica = new GestoreGrafica();
-		
+
 		Componente c = null;
 
 		do {
 			c = servizio.estraiComponente();
-			
-			if(c != null) {
-				
-				gestoreGrafica.stampa(grafica.rappresentaComponente(c)) ;
-			}
-		}while(c != null);
-		
-		
-		
-	}
 
+			if (c != null) {
+
+				gestoreGrafica.stampa(grafica.rappresentaComponente(c));
+			}
+		} while (c != null);
+	}
 }
