099e086f6a1bf8f7da53dda7a66e627d36b0d850
Matteo Baccanelli <m.baccanelli1@studenti.unibg.it>
2025-04-16 16:59:09 +0200

    Aggiunto metodo getModalita alla classe Inizializzazione

--- src/partita/fasiGioco/Inizializzazione.java
+++ src/partita/fasiGioco/Inizializzazione.java
@@ -1,13 +1,16 @@
 package partita.fasiGioco;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 import grafica.Colore;
-import io.GestoreInput;
-import io.GestoreOutput;
+import io.*;
 import model.Giocatore;
+import nave.TipoNave;
+import partita.LivelloPartita;
+import partita.ModalitaGioco;
 
 public class Inizializzazione {
 
@@ -17,13 +20,13 @@ public class Inizializzazione {
 	private GestoreInput gestoreInput;
 	private Scanner sc;
 
-	private static List<Colore> COLORI_RESTANTI;
+	private static List<Colore> COLORI_UTILIZZATI;
 
 	public Inizializzazione() {
 		gestoreOutput = new GestoreOutput();
 		gestoreInput = new GestoreInput();
-		sc = new Scanner(System.in); // TODO: usare il gestore input?
-		COLORI_RESTANTI = Arrays.asList(Colore.BLU, Colore.ROSSO, Colore.GIALLO, Colore.VERDE);
+		sc = new Scanner(System.in);
+		COLORI_UTILIZZATI = new ArrayList<>();
 	}
 
 	public Giocatore[] start() {
@@ -43,26 +46,73 @@ public class Inizializzazione {
 
 			Colore colore = null;
 			do {
-				gestoreOutput.stampa("Colori disponibili: " + COLORI_RESTANTI);
+				gestoreOutput.stampa("Colori disponibili: " + Giocatore.coloriDisponibiliGiocatori);
 				gestoreOutput.stampa("Scegli il colore del giocatore " + (i + 1));
 				String col = sc.nextLine().toUpperCase();
 
 				try {
 					colore = Colore.valueOf(col);
-					if (!COLORI_RESTANTI.contains(colore)) {
-						gestoreOutput.stampa("Scelta del colore non possibile.");
-						colore = null;
+					if (COLORI_UTILIZZATI.contains(colore)) {
+						gestoreOutput.stampa("Colore già scelto.");
+
 					}
 				} catch (IllegalArgumentException e) {
 					gestoreOutput.stampa("Colore non valido. Riprova.");
+				} finally {
+					colore = null;
 				}
 			} while (colore == null);
-			COLORI_RESTANTI.remove(colore);
+			COLORI_UTILIZZATI.add(colore);
 
 			giocatori[i] = new Giocatore(nome, colore);
 		}
 
 		sc.close();
 		return giocatori;
+	}
+
+	public ModalitaGioco getModalita() {
+		String scelta = "";
+		ModalitaGioco modalita = null;
+		gestoreOutput.stampa("Vuoi giocare a \"Volo Singolo\" o a \"Trasvolata Intergalattica\"?");
+		gestoreOutput.stampa("Scrivi semplicemente \"volo\" oppure \"trasvolata\".");
+		do {
+			scelta = sc.nextLine().toLowerCase();
+			switch (scelta) {
+			case "volo":
+				modalita = ModalitaGioco.VOLO_SINGOLO;
+				break;
+			case "trasvolata":
+				modalita = ModalitaGioco.TRASVOLATA_INTERGALATTICA;
+				break;
+			default:
+				gestoreOutput.stampa("Opzione non valida.");
+			}
+		} while (scelta == "");
+
+		if (modalita == ModalitaGioco.VOLO_SINGOLO) {
+			int livello = 0;
+			gestoreOutput.stampa("Scegli il livello del gioco, da 1 a 3.");
+			do {
+				livello = gestoreInput.leggiIntero();
+				switch (livello) {
+				case 1:
+					modalita.setlivelloPartita(LivelloPartita.LIVELLO_1);
+					break;
+				case 2:
+					modalita.setlivelloPartita(LivelloPartita.LIVELLO_2);
+					break;
+				case 3:
+					modalita.setlivelloPartita(LivelloPartita.LIVELLO_3);
+					break;
+				default:
+					gestoreOutput.stampa("Opzione non valida.");
+				}
+
+			} while (livello == 0);
+		} else
+			modalita.setlivelloPartita(LivelloPartita.LIVELLO_1);
+
+		return modalita;
 	}
 }
