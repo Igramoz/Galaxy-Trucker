7cdb5433fb96dcb5d59d45adf6c0b470ec985ab2
michele-bisignano <m.bisignano@studenti.unibg.it>
2025-04-17 20:12:56 +0200

    Definita la classe Carta, PioggiaDiMeteoriti, TipiMeteorite e definita la classe Coppia.

--- src/cartaAvventura/Carta.java
+++ src/cartaAvventura/Carta.java
@@ -1,5 +1,16 @@
 package cartaAvventura;
 
 public abstract class Carta {
+    private final TipoCarta tipoCarta;
+    
+    public Carta(TipoCarta tipoCarta) {
+    	this.tipoCarta = tipoCarta;
+    }
+    
+    public TipoCarta getTipoCarta() {
+    	return tipoCarta;
+    }
+    
+    abstract void eseguiEvento();
     
 }
