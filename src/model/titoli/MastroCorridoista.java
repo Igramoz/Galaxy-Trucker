335b15f69815977459d15b7ca97023f015d1f845
Matteo Baccanelli <m.baccanelli1@studenti.unibg.it>
2025-05-19 16:27:46 +0200

    Completamento titolo MastroCorridoista

--- src/model/titoli/MastroCorridoista.java
+++ src/model/titoli/MastroCorridoista.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import model.Giocatore;
 import model.nave.Nave;
+import util.layout.Coordinate;
 import util.layout.Direzione;
 import model.componenti.Componente;
 import model.enums.TipoTubo;
@@ -27,23 +28,26 @@ public class MastroCorridoista extends Titolo {
 		Componente[][] griglia = n.getGrigliaComponentiCloni();
 		for (Componente[] riga : griglia) {
 			for (Componente c : riga) {
-				if (c != null && isCorridoio(c))
+				if (isCorridoio(c))
 					listaComponentiCorridoio.add(c);
 			}
 		}
 
-//		int maxLunghezza = 0;
-//		for (Componente c : listaComponentiCorridoio) {
-//			int temp = calcolaLunghezzaCorridoio(c);
-//			if (temp > maxLunghezza)
-//				maxLunghezza = temp;
-//		}
-//		return maxLunghezza;
-		return calcolaLunghezzaCorridoio(n, listaComponentiCorridoio);
+		int maxLunghezza = 1;
+		for (Componente c : listaComponentiCorridoio) {
+			List<Coordinate> coordinateGiaEsaminate = new ArrayList<>();
+			int lunghezza = calcolaLunghezzaCorridoio(n, c, coordinateGiaEsaminate);
+			if (lunghezza > maxLunghezza)
+				maxLunghezza = lunghezza;
+		}
+		return maxLunghezza;
 	}
 
 	// un componente è un corridoio se il numero dei suoi tubi è esattamente 1 o 2
 	private boolean isCorridoio(Componente c) {
+		if (c == null)
+			return false;
+
 		int numTubi = 0;
 		Map<Direzione, TipoTubo> tubi = c.getTubi();
 		for (Direzione d : Direzione.values()) {
@@ -54,14 +58,22 @@ public class MastroCorridoista extends Titolo {
 		return numTubi == 1 || numTubi == 2;
 	}
 
-	// calcola e restituisce la lunghezza max del corridoio del componente
-	private int calcolaLunghezzaCorridoio(Nave n, List<Componente> listaComponentiCorridoio) {
-//		List<Coordinate> coordinateGiaEsaminate = new ArrayList<>();
-		int lunghezza = 0;
-		for (Componente c : listaComponentiCorridoio) {
-			Map<Direzione, Componente> adiacenti = n.getCopiaComponentiAdiacenti(c.getPosizione());
-			// TODO finire
+	// calcola e restituisce la lunghezza max del corridoio trovato
+	private int calcolaLunghezzaCorridoio(Nave n, Componente c, List<Coordinate> coordinateGiaEsaminate) {
+		Map<Direzione, Componente> adiacenti = n.getCopiaComponentiAdiacenti(c.getPosizione());
+		int maxLunghezza = 1;
+		coordinateGiaEsaminate.add(c.getPosizione());
+
+		for (Direzione d : Direzione.values()) {
+			Componente adiacente = adiacenti.get(d);
+			if (isCorridoio(adiacente) && !coordinateGiaEsaminate.contains(adiacente.getPosizione())) {
+				// copio la lista per evitare interferenze tra rami
+				List<Coordinate> copiaEsaminate = new ArrayList<>(coordinateGiaEsaminate);
+				int lunghezza = calcolaLunghezzaCorridoio(n, adiacente, copiaEsaminate);
+				if (lunghezza > maxLunghezza)
+					maxLunghezza = lunghezza;
+			}
 		}
-		return lunghezza;
+		return maxLunghezza;
 	}
 }
