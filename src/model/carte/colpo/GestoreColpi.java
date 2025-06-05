abd5b58b25040213bc0ef83e06fd1952ec0d83cb
michele-bisignano <m.bisignano@studenti.unibg.it>
2025-06-05 14:58:45 +0200

    modifiche a gestore colpi

--- src/model/carte/colpo/GestoreColpi.java
+++ src/model/carte/colpo/GestoreColpi.java
@@ -34,6 +34,7 @@ public class GestoreColpi {
 			for (ManagerDiVolo m : listaManager) {
 				Giocatore giocatore = m.getGiocatore();
 
+				io.aCapo();
 				io.stampa("stiamo valutando se la nave di " + formattatoreGrafico.formatta(giocatore) + " è stata colpita...");
 				// nave colpita
 				int pezziDistrutti = giocatore.getNave().subisciImpatto(colpo, posizioneColpo);
